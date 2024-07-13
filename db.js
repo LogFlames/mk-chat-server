e0d6aae2223565a29d040afd44216a6c445eab8a
Elias Lundell <elilun03@gmail.com>
2024-07-14 01:02:38 +0200

    init

---
+++ db.js
@@ -0,0 +1,59 @@
+const MongoClient = require('mongodb').MongoClient;
+const { v4: uuidv4 } = require('uuid');
+const url = "mongodb://localhost:27017";
+
+var _db;
+
+MongoClient.connect(url).then((client) => {
+    _db = client.db('mk-chat-server');
+    if (_db === undefined) {
+        console.log("No existing database"); 
+    } else {
+        console.log("Connected to database")
+    }
+}).catch((err) => {
+    throw err;
+});
+
+module.exports = {
+    createUser: async function(name, callback) {
+        let token = uuidv4();
+        let maxId = await _db.collection('users').find({},{projection:{id: true, _id: false}}).sort({id: -1}).limit(1).toArray();
+        if (maxId.length == 0) {
+            maxId = -1;
+        } else {
+            maxId = maxId[0]['id'];
+        }
+        _db.collection('users').insertOne({name: name, token: token, id: maxId + 1 });
+        callback(token);
+    },
+    deleteUser: function(id) {
+        _db.collection('users').findOneAndDelete({"id": +id});
+    },
+    deletePost: function(id) {
+        _db.collection('posts').findOneAndDelete({"id": id});
+    },
+    getAllUsers: function(callback) {
+        _db.collection('users').find({},{projection:{_id: false, token: false}}).toArray().then(result => {
+            callback(result)
+        });
+    },
+    getAllPosts: function(callback) {
+        _db.collection('posts').find({},{projection:{_id: false}}).toArray().then(result => {
+            callback(result);
+        });
+    },
+    getPost: async function(id) {
+        let posts = await _db.collection('posts').find({ id }).toArray();
+        return posts[0];
+    },
+    getUserFromToken: async function(token) {
+        let users = await _db.collection('users').find({ token }).toArray();
+        return users[0];
+    },
+    createPost: function(userid, message) {
+        let id = uuidv4();
+        _db.collection('posts').insertOne({id: id, userid: userid, message: message});
+        return id;
+    }
+}
---
+++ index.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').createServer(app);
+const cors = require('cors');
+const db = require('./db');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+const { v4: uuidv4 } = require('uuid');
+
+const SECRET_KEY = 'your_secret_key';
+const PASSWORD = '123'; // Replace with your desired password
+
+const authenticate = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (token) {
+        jwt.verify(token, SECRET_KEY, (err, decoded) => {
+            if (err) {
+                res.status(401).send('Unauthorized');
+            } else {
+                req.user = decoded;
+                next();
+            }
+        });
+    } else {
+        res.status(401).send('Unauthorized');
+    }
+};
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+app.post('/login', (req, res) => {
+    const { password } = req.body;
+
+    if (password === PASSWORD) {
+        const token = jwt.sign({ user: 'admin' }, SECRET_KEY, { expiresIn: '1h' });
+
+        res.cookie('token', token, { httpOnly: true, maxAge: 3600000 }); // 1 hour
+        res.status(200).send('Login successful');
+    } else {
+        res.status(401).send('Invalid password');
+    }
+});
+
+app.get('/login', (req, res) => {
+    let options = {
+        root: path.join(__dirname)
+    }
+    res.sendFile('login.html', options);
+});
+
+app.get('/admin', authenticate, (req, res) => {
+    let options = {
+        root: path.join(__dirname)
+    }
+
+    res.sendFile("admin.html", options);
+});
+
+app.post('/create-user', authenticate, (req, res) => {
+    const { name } = req.body;
+
+    db.createUser(name, (token) => {
+        res.status(200).send(token);
+    });
+});
+
+app.post('/delete-user', authenticate, (req, res) => {
+    const { id } = req.body;
+
+    db.deleteUser(id);
+    res.sendStatus(200);
+});
+
+app.post('/delete-post', authenticate, (req, res) => {
+    const { id } = req.body;
+
+    db.deletePost(id);
+    res.sendStatus(200);
+});
+
+app.get('/users', (req, res) => {
+    db.getAllUsers((data) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data, null, 1));
+    });
+});
+
+app.get('/posts', (req, res) => {
+    db.getAllPosts((data) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data, null, 1));
+    });
+});
+
+app.post('/post', (req, res) => {
+    const { token, message } = req.body;
+
+    if (token === undefined || message === undefined) {
+        return res.sendStatus(400);
+    }
+
+    let user = db.getUserFromToken(token);
+
+    if (user === undefined) {
+        return res.sendStatus(401);
+    }
+
+    let id = db.createPost(user.id, message);
+    res.status(200).send(id);
+});
+
+app.delete('/post', (req, res) => {
+    const { token, id } = req.body;
+
+    let user = db.getUserFromToken(token);
+    let post = db.getPost(id);
+
+    if (user === undefined) {
+        res.sendStatus(401);
+    }
+
+    if (post === undefined) {
+        res.sendStatus(404);
+    }
+
+    if (post.userid !== user.id) {
+        res.sendStatus(401);
+    }
+
+    db.deletePost(id);
+    res.sendStatus(200);
+});
+
+server.listen(8045, () => console.log("Lisening on port :8045"));
