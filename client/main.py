a942f10268e9dea280a72825e0b461f7c1af6eea
Elias Lundell <elilun03@gmail.com>
2024-07-15 23:45:33 +0200

    change url

--- client/main.py
+++ client/main.py
@@ -2,10 +2,22 @@ import requests
 
 from secret import TOKEN
 
-url = "http://localhost:8045/post"
+url = "https://chat.eliaslundell.se"
 
 message = input("Message: ")
 
-ret = requests.post(url, json = {"token": TOKEN, "message": message})
+ret = requests.post(url + "/post", json = {"token": TOKEN, "message": message})
+
+print(ret.text)
+
+ret = requests.get(url + "/posts", params = {"page": 0, "pageSize": 10})
+print(ret.text)
+
+if input("delete y/n?") != "y":
+    exit()
+
+i = input("id: ")
+
+ret = requests.delete(url + '/post', json = {"token": TOKEN, "id": i})
 
 print(ret)
